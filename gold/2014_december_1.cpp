425ec71f536295b96f51b14846bb5f54b5173b73
inluminas <87386149+inluminas@users.noreply.github.com>
2021-07-13 23:54:52 +0600

    Update 2014_december_1.cpp

--- gold/2014_december_1.cpp
+++ gold/2014_december_1.cpp
@@ -1,6 +1,6 @@
 /*
-	Author : inluminas
-	Problem: Guard Mark
+  Author : Pranshu
+  Problem: Guard Mark
 */
 #include"bits/stdc++.h"
 using namespace std;
@@ -10,43 +10,43 @@ using namespace std;
 #define endl "\n"
  
 struct cow{
-	ll height=0,weight=0,strength=0;
+  ll height=0,weight=0,strength=0;
 };
 const ll inf=1e18;
 const int lmt=(1<<20)+2;
 ll dp[lmt],weight[lmt],height[lmt];
 
 int main(){
-	fastio;
+  fastio;
 
-	//freopen("guard.in","r",stdin);
-	//freopen("guard.out","w",stdout);
+  //freopen("guard.in","r",stdin);
+  //freopen("guard.out","w",stdout);
 
-	int n,h;
-	cin>>n>>h;
-	cow a[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i].height>>a[i].weight>>a[i].strength;
-	}
-	for(int i=0;i<(1<<n);i++){
-		dp[i]=-inf;
-		for(int j=0;j<n;j++){
-			if(i&(1<<j)) weight[i]+=a[j].weight;
-			if(i&(1<<j)) height[i]+=a[j].height;
-		}
-	}
-	ll ans=-1;
-	for(int i=0;i<(1<<n);i++){
-		for(int j=0;j<n;j++){
-			if(i&(1<<j)) continue;
-			int id=i|(1<<j);
-			if(dp[i]==-inf) dp[i]=inf;
-			dp[id]=max(dp[id],max(min(dp[i]-a[j].weight,a[j].strength),min(a[j].strength-weight[i],dp[i])));
-			if(height[id]>=h) ans=max(ans,dp[id]);
-		}
-	}
-	if(ans==-1) cout<<"Mark is too tall"<<endl;
-	else cout<<ans<<endl;
+  int n,h;
+  cin>>n>>h;
+  cow a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i].height>>a[i].weight>>a[i].strength;
+  }
+  for(int i=0;i<(1<<n);i++){
+    dp[i]=-inf;
+    for(int j=0;j<n;j++){
+      if(i&(1<<j)) weight[i]+=a[j].weight;
+      if(i&(1<<j)) height[i]+=a[j].height;
+    }
+  }
+  ll ans=-1;
+  for(int i=0;i<(1<<n);i++){
+    for(int j=0;j<n;j++){
+      if(i&(1<<j)) continue;
+      int id=i|(1<<j);
+      if(dp[i]==-inf) dp[i]=inf;
+      dp[id]=max(dp[id],max(min(dp[i]-a[j].weight,a[j].strength),min(a[j].strength-weight[i],dp[i])));
+      if(height[id]>=h) ans=max(ans,dp[id]);
+    }
+  }
+  if(ans==-1) cout<<"Mark is too tall"<<endl;
+  else cout<<ans<<endl;
 
-	return 0;
+  return 0;
 } 
